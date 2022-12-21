aee27d4aee319b05206339ab4322dbf95b18c9bd
mcyandex <miguelcyandex130@gmail>
2022-12-21 22:53:36 +0100

    feat: added landing page with styling

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Footer } from './Footer'
+
+export const Home = () => {
+  return (
+    <>
+      <header>
+        <h1>Text-to-Image</h1>
+        <h2>- AI Image Generator -</h2>
+        <h3>Create Images by just entering a description</h3>
+        <br />
+        <button>TRY NOW!</button>
+    </header>
+    <Footer />
+    </>
+  )
+}
