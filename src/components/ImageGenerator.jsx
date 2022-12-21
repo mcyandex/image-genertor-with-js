e07cf2102d74154388b8274ea11aa2617491809f
mcyandex <miguelcyandex130@gmail>
2022-12-21 21:04:01 +0100

    feat: added image size selector

--- src/components/ImageGenerator.jsx
+++ src/components/ImageGenerator.jsx
@@ -1,29 +1,49 @@
 import React, { useState } from 'react'
 import { Configuration, OpenAIApi } from 'openai';
+import { MoonLoader } from 'react-spinners';
 
 export const ImageGenerator = () => {
-    const [prompt, setPrompt] = useState("")
-    const [size, setSize] = useState("")
-    const [imageURL, setImageURL] = useState("")
-    const [loading, setLoading] = useState(false)
-
-    const { Configuration, OpenAIApi } = require("openai");
+    // OpenAI configuration
     const configuration = new Configuration({
         apiKey: process.env.REACT_APP_API_KEY,
     });
 
     const openai = new OpenAIApi(configuration);
+    // OpenAI API config end
+
+
+    // setting state for data management - text to search and image size
+    const [data, setData] = useState({
+        prompt: "",
+        size: "",
+    })
+    const { prompt, size } = data;
+    
+    // image url and loading state manager
+    const [imageURL, setImageURL] = useState("")
+    const [loading, setLoading] = useState(false)
+
+
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setData(prevData => ({
+            ...prevData,
+            [name]: value
+        }))
+    }
 
     
     
-    
+    // image generator func
     const generateImage = async () => {
         setLoading(true)
         try{
             const response = await openai.createImage({
               prompt: prompt,
-              n: 2,
-              size: "1024x1024",
+              n: 1,
+              size: size,
             });
             setImageURL(response.data.data[0].url)
             setLoading(false)
@@ -33,24 +53,30 @@ export const ImageGenerator = () => {
         }
     }
 
+    if(loading){
+        return <MoonLoader />
+    }
 
 
   return (
     <div>
-        <div>
+        <div id='controllers'>
             <input 
             type="text" 
+            name='prompt'
             placeholder='type image detail'
-            onChange={(e) => setPrompt(e.target.value)}
+            value={prompt}
+            onChange={handleChange}
             />
-            <select name="" id="">
+            <select name="size" defaultValue="Select Image Size" onChange={handleChange} >
+                <option defaultValue="" value="Select Image Size" disabled>Select Size</option>
                 <option value="256x256">Small</option>
                 <option value="512x512">Medium</option>
                 <option value="1024x1024">Large</option>
             </select>
             <button type='button' onClick={generateImage}>GENERATE IMAGE</button>
         </div>
-        <div>
+        <div id='image'>
             <img src={imageURL} alt={prompt} />
         </div>
     </div>
