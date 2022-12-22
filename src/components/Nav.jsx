d067a95ec02c7a7109a8c6b77fcb4a4ef88cf6a3
mcyandex <miguelcyandex130@gmail>
2022-12-22 11:50:16 +0100

    feat: added nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/"><button>Generate Image</button></Link></li>
+            <li><Link to="/">Contact</Link></li>
+            <li><Link to="/">About</Link></li>
+        </ul>
+
+    </nav>
+  )
+}
