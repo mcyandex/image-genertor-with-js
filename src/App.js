88b4e6d6966c654ce395f4c9a947bb7c91423e76
mcyandex <miguelcyandex130@gmail>
2022-12-22 12:05:52 +0100

    feat:  added image generation page route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Home } from "./components/Home";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import { Footer } from "./components/Footer";
+import { ImageGenerator } from "./components/ImageGenerator";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/image-generator" element={<ImageGenerator />} />
       </Routes>
       <Footer />
     </Router>
