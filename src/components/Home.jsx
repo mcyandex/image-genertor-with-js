22184afd71d1180e2da6a1801df6b3a46a6e66c8
mcyandex <miguelcyandex130@gmail>
2022-12-22 12:00:11 +0100

    docs: changed page title description and removed footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Footer } from './Footer'
+import { Nav } from './Nav'
 
 export const Home = () => {
   return (
     <>
-      <header>
+    <Nav />
+    <header>
         <h1>Text-to-Image</h1>
         <h2>- AI Image Generator -</h2>
-        <h3>Create Images by just entering a description</h3>
+        <h3>Easily create realistic Images by just entering a description.</h3>
         <br />
         <button>TRY NOW!</button>
     </header>
-    <Footer />
     </>
   )
 }
